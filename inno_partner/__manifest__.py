54625c3d7401e08eda8596c638c8d7394eb648dc
dadnanf <dwiki@portcities.net>
2018-12-10 15:44:22 +0700

    [IMP] update manifest

--- inno_partner/__manifest__.py
+++ inno_partner/__manifest__.py
@@ -7,7 +7,11 @@
     'summary'       : 'Innograph Vendor',
     'sequence'      : 1,
     'description'   : """
-        
+    - Add new model Line of Business on Purchase \n
+    - Add new field Vendor Specialization on Partner \n
+    - Add new field Product Specialization on Partner \n
+    - Add new field Line of Business on Partner \n
+    - Add new field No PKP on Partner \n
         *Author : Dwiki Adnan F.
     """,
     'depends'       : ['purchase', 'efaktur'],
