a524d529568a069119f24f33c2a52807db5a42dc
Dionisius Marcellius from Portcities <dion@portcities.net>
2018-12-11 16:16:57 +0700

    [REF][ADD] partner validation

--- inno_partner/__manifest__.py
+++ inno_partner/__manifest__.py
@@ -1,24 +1,16 @@
 # -*- coding: utf-8 -*-
 {
     'name'          : 'Innograph Partner',
-    'version'       : '10.0.1.0',
-    "author"        : "Port Cities",
+    'version'       : '10.0.1.0.0',
+    'author'        : 'Port Cities',
     'category'      : 'Purchase',
     'summary'       : 'Innograph Vendor',
     'sequence'      : 1,
-    'description'   : """
-    - Add new model Line of Business on Purchase \n
-    - Add new field Vendor Specialization on Partner \n
-    - Add new field Product Specialization on Partner \n
-    - Add new field Line of Business on Partner \n
-    - Add new field No PKP on Partner \n
-        *Author : Dwiki Adnan F.
-    """,
-    'depends'       : ['purchase', 'efaktur'],
+    'depends'       : ['purchase', 'efaktur', 'sales_team'],
     'data'          : [
-                        'security/ir.model.access.csv',
-                        'views/res_partner_view.xml',
-                      ],
+        'security/ir.model.access.csv',
+        'views/res_partner_view.xml',
+    ],
     'installable'   : True,
     'application'   : True,
     'auto_install'  : False
