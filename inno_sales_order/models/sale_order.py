0fe8838f60970055c4276949e52bba59ef0f6b01
dadnanf <dwiki@portcities.net>
2018-12-20 13:31:58 +0700

    [IMP] Improve template report

--- inno_sales_order/models/sale_order.py
+++ inno_sales_order/models/sale_order.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
-from datetime import datetime
+from datetime import datetime, date
 from odoo.addons.amount_to_text_id.amount_to_text import Terbilang
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT as DTF, DEFAULT_SERVER_DATE_FORMAT as DF
 
 class SaleOrder(models.Model):
 	_inherit = "sale.order"
 
+	quotation_number = fields.Char(help="keep quotation number when change into Sales Order")
 	amount_text = fields.Char(compute='terbilang')
+	valid_days = fields.Integer(compute='_get_valid_day')
 
 	@api.model
 	def create(self, vals):
@@ -17,8 +20,10 @@ class SaleOrder(models.Model):
 	@api.multi
 	def action_confirm(self):
 		res = super(SaleOrder, self).action_confirm()
-		if self.state == 'sale':
-			self.name = self.env['ir.sequence'].with_context(force_company=self.company_id.id).next_by_code('inno.sale.order') or self.name	
+		for sale in self:
+			if sale.state == 'sale':
+				sale.quotation_number = sale.name
+				sale.name = self.env['ir.sequence'].with_context(force_company=self.company_id.id).next_by_code('inno.sale.order') or self.name	
 
 	def _get_contact_person(self):
 		contact = False
@@ -33,3 +38,13 @@ class SaleOrder(models.Model):
 	@api.one
 	def terbilang(self):
 		self.amount_text = Terbilang(self.amount_total)
+
+	def _get_valid_day(self):
+		diff = 0
+		if self.date_order and self.validity_date:
+			date_order = datetime.strptime(self.date_order, DTF).date()
+			year, month, day = self.validity_date.split('-')
+			validity_date = date(int(year), int(month), int(day))
+			delta = validity_date - date_order
+			diff = delta.days
+		self.valid_days = diff
