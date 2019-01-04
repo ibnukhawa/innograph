de3d7b309cf84457e6bc434e4033e0f32e1e884a
dadnanf <dwiki@portcities.net>
2019-01-04 10:48:35 +0700

    [IMP] Improvement on menu Sale Approval

--- inno_sale_approval/models/sale.py
+++ inno_sale_approval/models/sale.py
@@ -8,7 +8,7 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     approver_id = fields.Many2one('res.users', 'Approver', track_visibility='onchange', default=False)
-    is_need_approval = fields.Boolean('Need Approval', compute="_compute_need_approval")
+    is_need_approval = fields.Boolean('Need Approval', store=True, compute="_compute_need_approval")
 
     @api.onchange('amount', 'order_line.discount')
     def _onchange_order_discount(self):
@@ -22,24 +22,27 @@ class SaleOrder(models.Model):
         return {'domain' : {'approver_id': [('id', 'in', user_approval._ids)]}}
 
 
-    @api.onchange('amount', 'order_line.discount')
+    @api.multi
+    @api.depends('amount', 'order_line.discount')
     def _compute_need_approval(self):
-        user = self.env.user
-        if self.user_id:
-            user = self.user_id
-        
-        if not user.sale_order_can_approve:
-            self.is_need_approval = True
-        else:
-            discount_list = self.order_line.mapped('discount')
-            discount = 0
-            if any(discount_list):
-                discount = max(discount_list)
-
-            if discount > user.sale_order_discount_limit:
-                self.is_need_approval = True
+        for sale in self:
+            user = self.env.user
+            if sale.user_id:
+                user = sale.user_id
+            
+            if not user.sale_order_can_approve:
+                sale.is_need_approval = True
             else:
-                self.is_need_approval = False
+                discount_list = sale.order_line.mapped('discount')
+                discount = 0
+                if any(discount_list):
+                    discount = max(discount_list)
+
+                if discount > user.sale_order_discount_limit:
+                    sale.is_need_approval = True
+                else:
+                    sale.approver_id = False
+                    sale.is_need_approval = False
 
     
     @api.multi
