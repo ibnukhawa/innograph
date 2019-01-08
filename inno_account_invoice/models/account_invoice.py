fbca01d04c16e8220db952f281c34ee726316d5c
siti mawaddah <siti@portcities.net>
2019-01-08 13:50:22 +0700

    add condition and depends state

--- inno_account_invoice/models/account_invoice.py
+++ inno_account_invoice/models/account_invoice.py
@@ -11,20 +11,23 @@ class AccountInvoice(models.Model):
     sme_id = fields.Many2one('project.sme', string="SME", store=True, compute="_compute_project_code_sme")
 
     @api.multi
-    @api.depends('payment_ids', 'payments_widget')
+    @api.depends('payment_ids', 'payments_widget','state')
     def _compute_date_payment(self):
         for invoice in self:
-            payment_ids = invoice.payment_ids
-            payment_date = payment_ids.mapped('payment_date')
-            # Get payment data from Payments
-            if any(payment_date):
-                invoice.date_payment = max(payment_date)
-            # Get payment data from payment widget (Paid with Bank Statement)
-            elif invoice.payments_widget:
-                payment = json.loads(invoice.payments_widget)
-                if payment and any(payment.get('content', False)):
-                    content = payment.get('content')
-                    invoice.date_payment = content[0].get('date', False)
+            if invoice.state=='paid':
+                payment_ids = invoice.payment_ids
+                payment_date = payment_ids.mapped('payment_date')
+                # Get payment data from Payments
+                if any(payment_date):
+                    invoice.date_payment = max(payment_date)
+                # Get payment data from payment widget (Paid with Bank Statement)
+                elif invoice.payments_widget:
+                    payment = json.loads(invoice.payments_widget)
+                    if payment and any(payment.get('content', False)):
+                        content = payment.get('content')
+                        invoice.date_payment = content[0].get('date', False)
+            else:
+                invoice.date_payment=False
 
     @api.multi
     @api.depends('invoice_line_ids', 'invoice_line_ids.account_analytic_id')
