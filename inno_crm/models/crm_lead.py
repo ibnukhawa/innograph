fb8fe52836207951583d8f868971a734d2d82990
dadnanf <dwiki@portcities.net>
2019-01-10 10:02:30 +0700

    [IMP] Improve module inno_crm

--- inno_crm/models/crm_lead.py
+++ inno_crm/models/crm_lead.py
@@ -7,6 +7,9 @@ from datetime import datetime
 class CRMLead(models.Model):
 	_inherit = "crm.lead"
 
+	stage_id = fields.Many2one('crm.stage', string='Stage', track_visibility='onchange', index=True, 
+		group_expand='_read_group_stage_ids', default=lambda self: self._default_stage_id())
+
 	@api.multi
 	@api.onchange('lead_product_ids', 'lead_product_ids.price_unit')
 	def _onchange_products_price_unit(self):
@@ -20,13 +23,15 @@ class CRMLead(models.Model):
 	@api.multi
 	def write(self, vals):
 		if 'team_id' in vals:
-			stage_ids = self.env['crm.stage'].search([('team_id', '=', vals.get('team_id'))])
-			for stage in stage_ids:
+			stage_ids = self.env['crm.stage'].search([])
+			stage_ids = stage_ids.filtered(lambda x:vals.get('team_id') in x.team_ids._ids)
+			for stage in stage_ids.sorted('probability'):
 				vals['stage_id'] = stage.id
 				break
+		if 'probability' in vals:
+			prob = vals.get('probability')
+			stage_id = self.env['crm.stage'].search([('probability', '>=', prob)], order='probability', limit=1)
+			if stage_id:
+				vals['stage_id'] = stage_id.id
 		return super(CRMLead, self).write(vals)
-
-
-
-
 	
