4d520d19b2cc71694fd5c05ca7b8e010cb5385c5
dadnanf <dwiki@portcities.net>
2019-01-10 13:32:34 +0700

    [IMP] Improve codes

--- inno_crm/models/crm_lead.py
+++ inno_crm/models/crm_lead.py
@@ -1,37 +1,42 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields, api, _
-from odoo.exceptions import UserError
-from datetime import datetime
+""" Import """
+from odoo import models, fields, api
 
 
 class CRMLead(models.Model):
-	_inherit = "crm.lead"
+    """ Inherit CRM Lead"""
+    _inherit = "crm.lead"
 
-	stage_id = fields.Many2one('crm.stage', string='Stage', track_visibility='onchange', index=True, 
-		group_expand='_read_group_stage_ids', default=lambda self: self._default_stage_id())
+    stage_id = fields.Many2one('crm.stage', string='Stage',
+                               track_visibility='onchange', index=True,
+                               group_expand='_read_group_stage_ids',
+                               default=lambda self: self._default_stage_id())
 
-	@api.multi
-	@api.onchange('lead_product_ids', 'lead_product_ids.price_unit')
-	def _onchange_products_price_unit(self):
-		for lead in self:
-			total = 0
-			for line in lead.lead_product_ids:
-				total += (line.price_unit * line.qty)
-			lead.planned_revenue = total
+    @api.multi
+    @api.onchange('lead_product_ids', 'lead_product_ids.price_unit')
+    def _onchange_products_price_unit(self):
+        """ Function to get Revenue from Total Amount Product Line """
+        for lead in self:
+            total = 0
+            for line in lead.lead_product_ids:
+                total += (line.price_unit * line.qty)
+            lead.planned_revenue = total
 
-
-	@api.multi
-	def write(self, vals):
-		if 'team_id' in vals:
-			stage_ids = self.env['crm.stage'].search([])
-			stage_ids = stage_ids.filtered(lambda x:vals.get('team_id') in x.team_ids._ids)
-			for stage in stage_ids.sorted('probability'):
-				vals['stage_id'] = stage.id
-				break
-		if 'probability' in vals:
-			prob = vals.get('probability')
-			stage_id = self.env['crm.stage'].search([('probability', '>=', prob)], order='probability', limit=1)
-			if stage_id:
-				vals['stage_id'] = stage_id.id
-		return super(CRMLead, self).write(vals)
-	
+    @api.multi
+    def write(self, vals):
+        """
+            Extend write function to set stage based on Team/Probability
+        """
+        if 'team_id' in vals:
+            stage_ids = self.env['crm.stage'].search([])
+            stage_ids = stage_ids.filtered(lambda x: vals.get('team_id') in x.team_ids._ids)
+            for stage in stage_ids.sorted('probability'):
+                vals['stage_id'] = stage.id
+                break
+        if 'probability' in vals:
+            prob = vals.get('probability')
+            stage_id = self.env['crm.stage'].search([('probability', '>=', prob)],
+                                                    order='probability', limit=1)
+            if stage_id:
+                vals['stage_id'] = stage_id.id
+        return super(CRMLead, self).write(vals)
--- inno_crm/models/crm_stage.py
+++ inno_crm/models/crm_stage.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
+""" Import """
+from odoo import fields, models
 
-from odoo import api, fields, models
 
 class Stage(models.Model):
-	_inherit = 'crm.stage'
+    """ Inherit CRM Stage"""
+    _inherit = 'crm.stage'
 
-	team_ids = fields.Many2many('crm.team', 'crm_stage_team_rel', 'stage_id', 'team_id', 
-		string='Teams', ondelete='set null', 
-		help='Specific team that uses this stage. Other teams will not be able to see or use this stage.')
+    team_ids = fields.Many2many('crm.team', 'crm_stage_team_rel', 'stage_id', 'team_id',
+                                string='Teams', ondelete='set null',
+                                help='Specific team that uses this stage. \
+                                Other teams will not be able to see or use this stage.')
