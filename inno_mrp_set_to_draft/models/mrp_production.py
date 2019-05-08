b469f1d4144018847d4f829894932a541d7949ca
Dionisius Marcellius from Portcities <dion@portcities.net>
2019-05-08 08:32:55 +0000

    [IMP] state check

--- inno_mrp_set_to_draft/models/mrp_production.py
+++ inno_mrp_set_to_draft/models/mrp_production.py
@@ -9,12 +9,15 @@ class MrpProduction(models.Model):
     job_description = fields.Text()
 
     @api.multi
-    def set_draft(self):
-        self.write({'state': 'draft'})
+    @api.depends('move_raw_ids')
+    def _has_moves(self):
+        super(MrpProduction, self)._has_moves()
+        for mo in self:
+            mo.has_moves = any([mv.state != 'cancel' for mv in mo.move_raw_ids])
 
     @api.multi
-    def action_open_task(self):
-        return
+    def set_draft(self):
+        self.write({'state': 'draft'})
 
     @api.multi
     def action_open_project(self):
