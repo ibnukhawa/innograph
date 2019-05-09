90eed5740eccb79cd64a9441a474eddbc79129f2
dwikiaf <dwiki@portcities.net>
2019-05-09 16:05:45 +0700

    [IMP] Improvement module inno_mrp_set_to_draft

--- inno_mrp_set_to_draft/models/sale_order.py
+++ inno_mrp_set_to_draft/models/sale_order.py
@@ -3,28 +3,29 @@ from odoo import models, fields, api
 
 
 class SaleOrder(models.Model):
-	_inherit = 'sale.order'
+    _inherit = 'sale.order'
 
-	date_scheduled = fields.Datetime(string="Scheduled Date")
-	file_id = fields.Binary()
-	size_image = fields.Char()
-	size_frame = fields.Char()
-	size_print = fields.Char()
-	finising = fields.Char()
-	packing = fields.Char()
-	finishing_note = fields.Char()
+    date_scheduled = fields.Datetime(string="Scheduled Date")
+    file_id = fields.Binary(string="File Name")
+    size_image = fields.Char(string="Image Size")
+    size_frame = fields.Char(string="Frame Size")
+    size_print = fields.Char(string="Print Size")
+    finishing = fields.Char()
+    packing = fields.Char()
+    finishing_note = fields.Char(string="Note")
+
 
 
 class ProductTemplate(models.Model):
-	_inherit = 'product.template'
+    _inherit = 'product.template'
 
-	@api.model
-	def _get_default_route(self):
-		mto_mts_route = self.env.ref('stock_mts_mto_rule.route_mto_mts', raise_if_not_found=False)
-		buy_route = self.env.ref('purchase.route_warehouse0_buy', raise_if_not_found=False)
-		default_route = mto_mts_route.ids+buy_route.ids
-		if mto_mts_route:
-			return default_route
-		return []
+    @api.model
+    def _get_default_route(self):
+        mto_mts_route = self.env.ref('stock_mts_mto_rule.route_mto_mts', raise_if_not_found=False)
+        buy_route = self.env.ref('purchase.route_warehouse0_buy', raise_if_not_found=False)
+        default_route = mto_mts_route.ids + buy_route.ids
+        if mto_mts_route:
+            return default_route
+        return []
 
-	route_ids = fields.Many2many(default=lambda self: self._get_default_route())
+    route_ids = fields.Many2many(default=lambda self: self._get_default_route())
