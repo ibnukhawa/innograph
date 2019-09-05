b327b7959901297a7b9a409975d00676c4f91980
Alfif from Port Cities <alfif@portcities.net>
2019-09-05 10:26:47 +0700

    [IMP] improve after UAT with structure field and compute price to website and goverment

--- inno_lkpp_sync/models/product.py
+++ inno_lkpp_sync/models/product.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class ProductTemplate(models.Model):
@@ -14,9 +14,27 @@ class ProductTemplate(models.Model):
     lkpp_category_id = fields.Char(string='Category')
     lkpp_manufacturer_id = fields.Char(string='Manufacturer')
 
-    lkpp_sell_price = fields.Float(string='Sell Price')
-    lkpp_govt_price = fields.Float(string='Goverment Price')
+    # lkpp_sell_price = fields.Float(string='Sell Price')
 
+    lkpp_govt_price = fields.Float(string='Goverment Price',compute='_computed_price_goverment')
+    website_price_new = fields.Float(string='Sale Price',readonly=False)
+
+    list_price = fields.Float(string='Website Price',compute='_computed_price_website')
+
+
+    @api.depends("website_price_new")
+    def _computed_price_website(self):
+        for record in self:
+            price = record.website_price_new + (record.website_price_new * 0.1)
+
+            record.list_price = price
+
+    @api.depends("website_price_new")
+    def _computed_price_goverment(self):
+        for record in self:
+            price = record.website_price_new - (record.website_price_new * 0.06)
+
+            record.lkpp_govt_price = price
 
 class ProductUoM(models.Model):
     _inherit = 'product.uom'
