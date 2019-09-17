7b6fbb9c261dcf1020ba263944bcdd5d99fbac65
@satrio_pci <satrio@portcities.net>
2019-09-17 10:45:48 +0700

    [IMP] - custom footer for ecommerce website

---
+++ inno_web_footer/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Innograph Custom Website Footer',
+    'version': '10.0.1.0',
+    "author": "Port Cities",
+    'category': 'eCommerce',
+    'summary': 'Customizing eCommerce website footer',
+    'description': """
+        *Author : SWS <satrio@portcities.net>
+    """,
+    'depends': [
+        'website_sale',
+        'theme_stoneware',
+    ],
+    'data': [
+        'views/templates.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
