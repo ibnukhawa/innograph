58c9812de0280ea71bfe6e8f1498a1edda4fc36d
saiful_pci <saiful@portcities.net>
2019-10-07 14:44:18 +0700

    imp sum qty sold

--- inno_web_product_details/__manifest__.py
+++ inno_web_product_details/__manifest__.py
@@ -12,10 +12,13 @@
         'theme_stoneware',
         'product',
         'website',
+	'website_sale',
     ],
     'data': [
         'views/product_template_view.xml',
+        'views/templates.xml',
         'views/web_product.xml',
+        'views/website.xml',
     ],
     'installable': True,
     'application': True,
