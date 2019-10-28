04b1f022f3bade466827cc297344ed5d4de3d5e4
Alfif from Port Cities <alfif@portcities.net>
2019-10-28 21:11:13 +0700

    [IMP] in detail mobile view and website view then error fixing when create invoice

--- pqm_website_access/models/account_invoice.py
+++ pqm_website_access/models/account_invoice.py
@@ -9,11 +9,11 @@ class AccountInvoice(models.Model):
 
 
 class SaleAdvancePaymentInv(models.TransientModel):
-    _inherit = "sale.advance.payment.inv"
-
-    @api.multi
-    def _create_invoice(self, order, so_line, amount):
-    	invoice = super(SaleAdvancePaymentInv, self)._create_invoice(order, so_line, amount)
+	_inherit = "sale.advance.payment.inv"
+	
+	@api.multi
+	def _create_invoice(self, order, so_line, amount):
+		invoice = super(SaleAdvancePaymentInv, self)._create_invoice(order, so_line, amount)
 		if invoice:
 			invoice.write({'origin_url': order.origin_url})
-    	return invoice
+		return invoice
