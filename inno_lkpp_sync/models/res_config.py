cc41c03362b8cc1cc68d1a744a265999db61b9e7
siti mawaddah <siti@portcities.net>
2019-11-09 23:17:39 +0700

    update lkpp

--- inno_lkpp_sync/models/res_config.py
+++ inno_lkpp_sync/models/res_config.py
@@ -20,7 +20,7 @@ class ResConfigSettings(models.TransientModel):
             'lkpp_manufacture_key': self.env['ir.config_parameter'].sudo().get_param('lkpp.manufacture_key', default=''),
             'lkpp_currency_key': self.env['ir.config_parameter'].sudo().get_param('lkpp.currency_key', default=''),
             'lkpp_secretkey': self.env['ir.config_parameter'].sudo().get_param('lkpp.secretkey', default=''),
-            'id_penawaran_lkpp': self.env['ir.config_parameter'].sudo().get_param('lkpp.penawaran_id', default='')
+            'id_penawaran_lkpp': self.env['ir.config_parameter'].sudo().get_param('lkpp.penawaran_id', default=''),
         }
 
     @api.multi
@@ -29,5 +29,6 @@ class ResConfigSettings(models.TransientModel):
         self.env['ir.config_parameter'].sudo().set_param('lkpp.uom_key', (self.lkpp_uom_key or '').strip(), groups=['base.group_system'])
         self.env['ir.config_parameter'].sudo().set_param('lkpp.manufacture_key', (self.lkpp_manufacture_key or '').strip(), groups=['base.group_system'])
         self.env['ir.config_parameter'].sudo().set_param('lkpp.currency_key', (self.lkpp_currency_key or '').strip(), groups=['base.group_system'])
+        self.env['ir.config_parameter'].sudo().set_param('lkpp.penawaran_id', (self.id_penawaran_lkpp or '').strip(), groups=['base.group_system'])        
         self.env['ir.config_parameter'].sudo().set_param('lkpp.secretkey', (self.lkpp_secretkey or '').strip(), groups=['base.group_system'])
-        self.env['ir.config_parameter'].sudo().set_param('lkpp.penawaran_id', (self.id_penawaran_lkpp or '').strip(), groups=['base.group_system'])
+  
