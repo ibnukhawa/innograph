9f45ee6aac46042eb9c37b1b9cc11af429c5fe5c
Alfif from Port Cities <alfif@portcities.net>
2019-11-11 19:36:13 +0700

    [add] multiwebsite with out blog slider

--- inno_web_home/static/src/js/custom.js
+++ inno_web_home/static/src/js/custom.js
@@ -364,9 +364,16 @@ function createBanner3(){
     });
 }
 
-createBanner3();
+function getLogo(){
+    $.get("/API/load_logo/"+u.host, function(data){
+        console.log('data logo ',data[0].url_logo)
+        $(".logos").attr("src", data[0].url_logo);
+        $(".inno-logo").attr("src", data[0].url_logo);
+    });
+}
 
-    
+createBanner3();
+getLogo();
 
 
     $.get("/API/load_category/"+u.host, function(data){
