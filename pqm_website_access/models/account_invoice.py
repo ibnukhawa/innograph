1da3fdb6e83f7522bd6c091198b52db35bbfc9ce
siti mawaddah <siti@portcities.net>
2019-11-11 12:07:28 +0700

    remove conflict invoice

--- pqm_website_access/models/account_invoice.py
+++ pqm_website_access/models/account_invoice.py
@@ -9,16 +9,6 @@ class AccountInvoice(models.Model):
 
 
 class SaleAdvancePaymentInv(models.TransientModel):
-<<<<<<< Upstream, based on origin/development
-	_inherit = "sale.advance.payment.inv"
-	
-	@api.multi
-	def _create_invoice(self, order, so_line, amount):
-		invoice = super(SaleAdvancePaymentInv, self)._create_invoice(order, so_line, amount)
-		if invoice:
-			invoice.write({'origin_url': order.origin_url})
-		return invoice
-=======
     _inherit = "sale.advance.payment.inv"
 
     @api.multi
@@ -27,4 +17,3 @@ class SaleAdvancePaymentInv(models.TransientModel):
     	if invoice:
     		invoice.write({'origin_url': order.origin_url})
     	return invoice
->>>>>>> e0e06af [IMP] Fix issue indentation
