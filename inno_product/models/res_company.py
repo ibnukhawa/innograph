a6d118cab4794c12c27150c31fde76b0d62cd738
dwikiaf <dwiki@portcities.net>
2020-01-10 10:18:53 +0700

    [IMP] Improvement on Finished goods default location

--- inno_product/models/res_company.py
+++ inno_product/models/res_company.py
@@ -6,14 +6,3 @@ class ResCompany(models.Model):
 
 	default_finished_product_location = fields.Many2one('stock.location')
 
-
-class StockSettings(models.TransientModel):
-    _inherit = 'stock.config.settings'
-
-    default_finished_product_location = fields.Many2one('stock.location', "Finished Product Location")
-
-
-    @api.onchange('default_finished_product_location')
-    def onchange_default_finished_product_location(self):
-    	if self.default_finished_product_location and self.company_id:
-    		self.company_id.write({'default_finished_product_location': self.default_finished_product_location.id})
