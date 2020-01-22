5f19e96678a5dcd58e5090532d3101fd9f2ff0e5
fakhry <fakhry@portcities.net>
2020-01-22 11:57:50 +0700

    [ADD] Adjustment decimal precision currency rate

--- pci_base_currency/models/res_currency_rate.py
+++ pci_base_currency/models/res_currency_rate.py
@@ -1,12 +1,4 @@
-import json
-import math
-import re
-import time
-
 from odoo import api, fields, models, tools, _
-
-CURRENCY_DISPLAY_PATTERN = re.compile(r'(\w+)\s*(?:\((.*)\))?')
-
 
 class CurrencyRate(models.Model):
     _inherit = "res.currency.rate"
