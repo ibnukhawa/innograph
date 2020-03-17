6f59e841d6c4debc629f0933b0e08a392c7ed58a
Alfif from Port Cities <alfif@portcities.net>
2020-03-17 11:10:15 +0700

    [ADD] module for favicon multi website, change font size at mega menus, add blank target in link follows footer

---
+++ inno_web_favicon/models/favicon.py
@@ -0,0 +1,28 @@
+from odoo import api, fields, models
+from odoo.http import request
+
+
+class WebsiteMenuUrl(models.Model):
+    _name = 'website.menu.url'
+
+    favicon = fields.Binary(string="Favicon")
+
+class Website(models.Model):
+	_inherit = 'website'
+
+	def getFavicon(self):
+		
+		url = request.httprequest.url_root
+		url_new = url.replace("http://","")
+		url_final = url_new[:-1]
+
+		domain=[
+			('url','=',url_final)
+		]
+
+		menu_url = request.env['website.menu.url'].search(domain)
+		images= '/web/image/website.menu.url/'+str(menu_url.id)+'/logo_favicon/100x100'
+		return images
+
+
+
--- inno_web_home/models/website.py
+++ inno_web_home/models/website.py
@@ -8,17 +8,3 @@ class Website(models.Model):
     _inherit = 'website'
 
     social_instagram = fields.Char('Instagram Account')
-
-    def getFavicon(self):
-        url = request.httprequest.url_root
-        url_new = url.replace("http://","")
-        url_final = url_new[:-1]
-
-        domain = [
-            ('url', '=', url_final)
-        ]
-        menu_url = request.env['website.menu.url'].search(domain)
-        images = '/web/image/website.menu.url/'+str(menu_url.id)+'/logo_footer/100x100'
-        
-        return images
-
