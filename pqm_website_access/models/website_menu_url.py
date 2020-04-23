52df8365d844415707ce4f3ca262cbcb0d301ae6
Alfif from Port Cities <alfif@portcities.net>
2020-04-23 08:37:08 +0700

    [IMP] change style at tabs product detail and change dynamic favicon

--- pqm_website_access/models/website_menu_url.py
+++ pqm_website_access/models/website_menu_url.py
@@ -9,3 +9,5 @@ class WebsiteMenuUrl(models.Model):
     url = fields.Char()
     logo = fields.Binary(string="Logo Header")
     logo_footer = fields.Binary(string="Logo Footer")
+    favicon = fields.Binary(string="Favicon")
+    website_title = fields.Char(string="Title")
